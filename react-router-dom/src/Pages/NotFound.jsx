dda00821d97940a6e6a44a1f09df551db42cbf18
rokan <rokansubhi.f@gmail.com>
2023-09-26 13:36:44 +0700

    react

---
+++ react-router-dom/src/Pages/NotFound.jsx
@@ -0,0 +1,11 @@
+
+
+function NotFound() {
+    return (  
+        <div className="not-found">
+            <h1>404</h1><br/>
+        </div>
+    );
+}
+
+export default NotFound;
---
+++ react-router-dom/src/components/Second.jsx
@@ -0,0 +1,11 @@
+
+
+const Second = () => {
+    return (
+        <div>
+            <h1>ABOUT PAGE</h1>
+        </div>
+        )
+}
+
+export default Second;
